d892ba02615158ea04f9fe8c843339a6eeea5e8c
Rola-saleh <143877384+Rola-saleh@users.noreply.github.com>
2025-03-23 08:53:24 +0300

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+echo "Enter the principal amount:"
+read p
+echo "Enter the annual interest rate (in %):"
+read r
+echo "Enter the time period (in years):"
+read t
+
+si=$(echo "scale=2; $p * $r * $t / 100" | bc)
+echo "The simple interest is: $si"
 
